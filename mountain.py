2727fab39921e807637ae55a12bd0acd032e8485
Alex M. M <acdenissk69@gmail.com>
2021-02-23 10:05:43 +0100

    Increment year in copyright notice

--- mountain.py
+++ mountain.py
@@ -2,7 +2,7 @@
 
 # MIT License
 #
-# Copyright (c) 2020 Alex M. M.
+# Copyright (c) 2021 Alex M. M.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
